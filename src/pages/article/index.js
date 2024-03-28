45a9c9a738824cbd6379612c883d222a09d84800
MD Rayhan Howlader <rhowlader5314@gmail.com>
2024-03-28 12:03:04 +0800

    updated routing

---
+++ src/pages/article/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArticlePage = () => {
+  return (
+    <div>
+      <h1>Welcome to article home page</h1>
+    </div>
+  )
+}
+
+export default ArticlePage
---
+++ src/pages/products/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductsHomePage = () => {
+  return (
+    <div>
+      <h1>this is proucts page</h1>
+    </div>
+  )
+}
+
+export default ProductsHomePage
